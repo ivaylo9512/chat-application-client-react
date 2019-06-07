331af3036750fba4541be9e19c9f090294c7ec36
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-07 12:16:49 +0300

    profile component added

---
+++ src/components/Profile.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Profile extends Component{
+    render(){
+        return (
+            <section>
+                <div class="image-container">
+                    <img/>
+                </div>
+                <div class="info">
+                </div>
+                <div class="buttons">
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+            </section>
+        )
+
+    }
+}
+
+export default Profile
