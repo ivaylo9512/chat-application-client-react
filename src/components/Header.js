652f1c60f5c5af52ac506dc337f8509f145c93cf
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-07 11:17:43 +0300

    app-component added

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+    render(){
+        return (
+            <header>
+                <nav>
+                    <input type="text">
+                    </input>
+                    <a></a>
+                    <a></a>
+                </nav>
+                <div className="bar">
+                </div>
+                <div className="chats-container">
+                </div>
+            </header>
+        )
+
+    }
+}
+
+export default Header
