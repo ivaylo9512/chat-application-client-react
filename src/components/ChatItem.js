aaae9a53c68e181332fd03180c708f762d1cd86e
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-07 11:37:37 +0300

    ChatItem component added

---
+++ src/components/ChatItem.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class ChatItem extends Component{
+    render(){
+        return (
+            <div>
+                <img/>
+                <div>
+                    <br></br>
+                    <span></span>    
+                </div>
+            </div>
+        )
+
+    }
+}
+
+export default ChatItem
