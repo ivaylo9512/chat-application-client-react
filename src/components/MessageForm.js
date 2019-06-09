47d467a4f709b9e6b02ef459e30238e02e808d41
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-09 17:08:48 +0300

    message form component added

---
+++ src/components/MessageForm.js
@@ -0,0 +1,22 @@
+import React,{ component } from 'react'
+
+class MessageForm extends Comment {
+    constructor() {
+        this.submitForm = this.submitForm.bind(this)
+    }
+    
+    submitForm(e) {
+
+    }
+
+    render() {
+        return (
+            <form onSubmit="this.submitForm">
+                <input placeholder="Send message"/>
+                <button>send</button>
+            </form>
+        )
+    }
+}
+
+export default MessageForm
