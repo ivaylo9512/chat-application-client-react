726e0199923a302e091f6bb9f2912e3da734e4ee
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-07-10 19:51:07 +0300

    websockets - subscribing and publishing

--- src/components/WebSocket.js
+++ src/components/WebSocket.js
@@ -7,24 +7,38 @@ class WebSocket extends Component{
         const client = new Client({
             brokerURL: "ws://localhost:8080/api/chat/message",
             connectHeaders: {
-              'Authorization': 'user',
+                'Authorization': localStorage.getItem('Authorization'),
             },
             debug: function (str) {
-              console.log(str)
+                console.log(str)
             },
             reconnectDelay: 5000,
             heartbeatIncoming: 4000,
             heartbeatOutgoing: 4000
-        });
-          client.onConnect = (frame) => {
-              console.log(frame)
-          };
+        })
+        client.onConnect = (frame) => {
+            this.props.setWebSocketClient(client)
+
+            let newMessage = function(message) {
+                console.log(message.body)
+            }
+            let newChat = function(message) {
+                console.log(message.body)
+            }
+            const createSubscription = client.subscribe("/user/message", newMessage);
+            const messageSubscription = client.subscribe("/user/createChat", newChat);
+
+            setTimeout(() => {
+                client.publish({destination: '/api/message', body: JSON.stringify({chatId: 22,receiverId: 6, message: "hey", username: "admin"}), headers: {'Authorization': localStorage.getItem('Authorization')}});
+                client.publish({destination: '/api/createChat', body: 6, headers: {'Authorization': localStorage.getItem('Authorization')}});
+            }, 3000);
+        };
        
-          client.onStompError = (frame) => {
+        client.onStompError = (frame) => {
             console.log('Broker reported error: ' + frame.headers['message']);
             console.log('Additional details: ' + frame.body);
-          };
-          client.activate();
+        }
+        client.activate();
     }
     render(){
         return (
