f0a44938075438da7f57aa0f6012d0f7882afca2
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-07-28 12:45:47 +0300

    chat logic moved to main

--- src/App.js
+++ src/App.js
@@ -2,21 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header'
 import Login from './components/Login'
-import SearchUsers from './components/SearchUsers';
 import ChatUsersList from './components/ChatUsersList';
-import SearchChat from './components/SearchChat';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
-import UsersList from './components/UsersList';
-import Chat from './components/Chat'
-import WebSocket from './components/WebSocket'
-import MessageForm from './components/MessageForm'
 import Main from './components/Main';
 
 class App extends Component {
     state = {
-        currentChat: undefined,
-        isUserPane: false,
-        foundUsers: [],
         user: undefined,
         chats: [],
         filteredChats: [],
@@ -60,22 +51,6 @@ class App extends Component {
         }) 
     }
 
-    setWebSocketClient = (client) => {
-        client.subscribe("/user/message", this.recievedMessage)
-        client.subscribe("/user/createChat", this.recievedNewChat)
-        
-        this.client = client
-    }
-
-    sendNewMessage = (message) => { 
-        const chatId = this.state.currentChat.id
-        const receiverId = this.state.currentChat.user.id
-        this.client.publish({destination: '/api/message', body: JSON.stringify({chatId, receiverId, message}), headers: {'Authorization': localStorage.getItem('Authorization')}});
-    }
-
-    createNewChat = (userId) => {
-        console.log("hey")
-    }
     recievedMessage = (message) => {
         console.log(message.body)
     }
@@ -90,17 +65,6 @@ class App extends Component {
         })
     }
 
-    removeCurrentChat = () => {
-        this.setState({
-            currentChat: undefined
-        })
-    }
-
-    setFoundUsers = (users) => {
-        this.setState({
-            foundUsers: users
-        })
-    }
 
     componentDidMount(){
         this.setState({ 
@@ -111,23 +75,12 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                {this.state.isAuth && <WebSocket setWebSocketClient={this.setWebSocketClient} />}
                 <Router>
                     {this.state.isAuth && <ChatUsersList setChatList={chatList => (this.chatList = chatList)} setCurrentChat={this.setCurrentChat} setUserChats={this.setUserChats} chats={this.state.filteredChats} />}
-                    <Route path="/login" render={() => <Login setUser={this.setUser} />} />
-                    <Route path="/searchUsers" render={() => <SearchUsers removeCurrentChat={this.removeCurrentChat} setFoundUsers={this.setFoundUsers} />} />
-                    <Route path="/searchUsers" render={({history}) => <UsersList  history={history} createNewChat={this.createNewChat} foundUsers={this.state.foundUsers} />} />
-              
-                    {this.state.currentChat !== undefined && 
-                    <div>
-                        <Route path="/chat" render={() => <Chat removeCurrentChat={this.removeCurrentChat} chat={this.state.currentChat} />} />
-                        <Route path="/chat" render={() => <MessageForm sendNewMessage={this.sendNewMessage} />} />
-                    </div>}
-                    <Route path="/chat" render={() => <SearchChat searchChats={this.searchChats}/>} />
-                 
+                    <Route path="/login" render={() => <Login setUser={this.setUser} />} />                               
                     <div className="content">
                         <Header chatList={this.chatList} logout={this.logout} />
-                        <Main />
+                        <Main isAuth={this.state.isAuth} searchChats={this.searchChats} currentChat={this.state.currentChat}/>
                     </div>
                 </Router>
             </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react'
+import WebSocket from './WebSocket'
+import UsersList from './UsersList';
+import SearchUsers from './SearchUsers';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import SearchChat from './SearchChat';
+import Chat from './Chat'
+import MessageForm from './MessageForm'
 
 class Main extends Component{
 
+    state = {
+        currentChat: undefined,
+        foundUsers: [],
+        chats: [],
+        filteredChats: [],
+    }
+
+    setWebSocketClient = (client) => {
+        client.subscribe("/user/message", this.recievedMessage)
+        client.subscribe("/user/createChat", this.recievedNewChat)
+        
+        this.client = client
+    }
+
+    
+    removeCurrentChat = () => {
+        this.setState({
+            currentChat: undefined
+        })
+    }
+
+    setFoundUsers = (users) => {
+        this.setState({
+            foundUsers: users
+        })
+    }
+
+    createNewChat = (userId) => {
+        console.log("hey")
+    }
+
+    sendNewMessage = (message) => { 
+        const chatId = this.state.currentChat.id
+        const receiverId = this.state.currentChat.user.id
+        this.client.publish({destination: '/api/message', body: JSON.stringify({chatId, receiverId, message}), headers: {'Authorization': localStorage.getItem('Authorization')}});
+    }
+    
     render(){
         return(
             <div className="messages-container">
-                <span>No chat is selected!</span>
+                {this.props.isAuth && <WebSocket setWebSocketClient={this.setWebSocketClient} />}
+         
+                <Route path="/searchUsers" render={() => <SearchUsers removeCurrentChat={this.removeCurrentChat} setFoundUsers={this.setFoundUsers} />} />
+                <Route path="/searchUsers" render={({history}) => <UsersList  history={history} createNewChat={this.createNewChat} foundUsers={this.state.foundUsers} />} />
+                <Route path="/searchChat" render={() => <SearchChat searchChats={this.props.searchChats}/>} />
+
+                {
+                    this.props.currentChat !== undefined ? 
+                        <div>
+                            <Route path="/chat" render={() => <Chat removeCurrentChat={this.removeCurrentChat} chat={this.props.currentChat} />} />
+                            <Route path="/chat" render={() => <MessageForm sendNewMessage={this.sendNewMessage} />} />
+                        </div> :
+                        <p>No chat is selected!</p>
+                }
             </div>
         )
     }
