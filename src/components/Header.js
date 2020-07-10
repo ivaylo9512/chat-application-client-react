36eae5d19f8eb2335aa4951539e4bc954e79b324
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-07-10 20:07:02 +0300

    header component from class to function with hooks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,42 @@
-import React, {Component} from 'react';
-import {Link, Route} from 'react-router-dom'
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom'
 
-class Header extends Component{
+const Header = ({chats, logout}) => {
+    const header = React.useRef()
+    const menuCircle = React.useRef()
 
-    constructor(){
-        super()
-        this.header = React.createRef()
-        this.menuCircle = React.createRef()
+    const hideMenu = () => {
+        header.current.classList.add('hide')
+        menuCircle.current.classList.add('show')
     }
 
-    hideMenu = () => {
-        this.header.current.classList.add('hide')
-        this.menuCircle.current.classList.add('show')
-    }
-
-    showHeader = () => {
-        this.header.current.classList.remove('hide')
-        this.menuCircle.current.classList.remove('show')
+    const showHeader = () => {
+        header.current.classList.remove('hide')
+        menuCircle.current.classList.remove('show')
         
     }
 
-    toggleChats = () => {
-        this.props.chats.current.classList.toggle('hide')
+    const toggleChats = () => {
+        chats.current.classList.toggle('hide')
     }
 
-    render(){
-        return (
-            <div ref={this.header} className="header-container">
-                <button className="menu-circle" onClick={this.showHeader} ref={this.menuCircle}><i className="fas fa-bars"></i></button>
-                <header>
-                    <button onClick={this.hideMenu} className="circle-btn">-</button>
-                    <div className="circle-nav" id="cn-wrapper">
-                        <ul>
-                            <li><span><i className="fas fa-user"></i></span></li>
-                            <li><span onClick={this.toggleChats}><i className="fas fa-comments"></i></span></li>
-                            <li><Link to="/searchChat"><i className="fas fa-search"></i></Link></li>
-                            <li><Link to="/searchUsers"><i className="fas fa-user-plus"></i></Link></li>
-                            <li><span onClick={this.props.logout}><i className="fas fa-sign-out-alt"></i></span></li>
-                        </ul>
-                    </div>
-                </header>
-            </div>
-        )
-
-    }
+    return (
+        <div ref={header} className="header-container">
+            <button className="menu-circle" onClick={showHeader} ref={menuCircle}><i className="fas fa-bars"></i></button>
+            <header>
+                <button onClick={hideMenu} className="circle-btn">-</button>
+                <div className="circle-nav" id="cn-wrapper">
+                    <ul>
+                        <li><span><i className="fas fa-user"></i></span></li>
+                        <li><span onClick={toggleChats}><i className="fas fa-comments"></i></span></li>
+                        <li><Link to="/searchChat"><i className="fas fa-search"></i></Link></li>
+                        <li><Link to="/searchUsers"><i className="fas fa-user-plus"></i></Link></li>
+                        <li><span onClick={logout}><i className="fas fa-sign-out-alt"></i></span></li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+    )
 }
 
 export default Header
