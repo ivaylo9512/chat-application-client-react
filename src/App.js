27d4bc77850f3ec3caa66cce18dd536f6e289c1a
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-07-14 23:48:46 +0300

    app type structure added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom'
 
 const App = () => {
     const [user, setUser] = useState(undefined)
-    const [auth, setAuth, removeAuth] = useLocalStorage('Authorization', null);
+    const [auth, setAuth, removeAuth] = useLocalStorage('Authorization', null)
+    const [appType, setAppType] = useLocalStorage('appType', null)
 
     const logout = () => {
         setUser(null)
@@ -19,11 +20,18 @@ const App = () => {
             <Router>
                 {auth ? 
                     <>
-                        <Logged logout={logout} user={user} /> 
+                        <Logged logout={logout} user={user} appType={appType} /> 
                         <Redirect from="login" to="/" />
-                    </>:
+                    </> :
                     <Route path="/login" render={() => 
-                        <Login setUser={setUser} setAuth={setAuth} />} />                               
+                        appType ? 
+                            <Login setUser={setUser} setAuth={setAuth} /> :
+                            <div className="type-buttons">
+                                <button onClick={() => setAppType('chatOnly')} />
+                                <button onClick={() => setAppType("restaurant")} />
+                            </div> 
+                    } 
+                    />                               
                 }
             </Router>
         </div>
