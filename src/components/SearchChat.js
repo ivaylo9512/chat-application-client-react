72520daea1261afe53dd5388b26354029c306e83
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-07-15 16:51:54 +0300

    SearchChat small fixes for input and submit + form style

--- src/components/SearchChat.js
+++ src/components/SearchChat.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useInput } from '../hooks/useInput' 
-const SearchChat = searchChats => {  
-
+const SearchChat = ({searchChats}) => {  
     const [name, setName, nameInput] = useInput({type: 'text', placeholder:'search chat'})
+    
+    const search = (e) => {
+        e.preventDefault()
+        searchChats(name)
+    }
     
     return (
         <div className='form-container'>
-            <form onSubmit={() => searchChats(name)}>
+            <form onSubmit={search}>
                 {nameInput}
                 <button><i className='fas fa-search'></i></button>
             </form>
