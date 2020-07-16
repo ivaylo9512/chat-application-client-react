010216f9fa91e48785b84f282dc1d77a80205883
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-07-16 17:32:40 +0300

    Register change in server call moved to useEffect

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,12 @@
-import React, {Component} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useInput } from '../hooks/useInput';
 
 const Register = ({setUser}) => {
     const [username, setUsername, usernameInput] = useInput('text', 'username') 
     const [password, setPassword, passwordInput] = useInput('password', 'password') 
     const [repeat, setRepeat, repeatInput] = useInput('password', 'repeat') 
-
+    const [userInfo, setUserInfo] = useState(undefined)
+    const [setError]
     register = (e) => {
         e.preventDefault()
         fetch('http://localhost:8080/api/users/register', {
@@ -18,9 +19,34 @@ const Register = ({setUser}) => {
           }
         )
     }
+    const setInfo = () => {
+        e.preventDefault()
+        setUserInfo({username, password, repeat})
+    }
+
+    useEffect(() => {
+        let isCurrent = true;
+        if(user){
+            const response = await fetch('http://localhost:8080/api/users/register', {
+                method: 'post',
+                body: JSON.stringify(userInfo)
+            })
+            const data = await response.text()
+            if(response.ok){
+                setUser(JSON.parse(data))
+            }else{
+                setError(data)
+            }
+            register()
+        }
+        return () => {
+            isCurrent = false
+        }
+    }, [userInfo])
+
     return (
         <section>
-            <form onSubmit={register}>
+            <form onSubmit={setInfo}>
                 {usernameInput}
                 {passwordInput}
                 {repeatInput}
