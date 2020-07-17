085cf30ed97b55f2d31de2a01f723adfb12f0966
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-07-18 02:48:45 +0300

    Profile + UsersList to functional

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,23 +1,20 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Profile extends Component{
-    render(){
-        return (
-            <section>
-                <div class='image-container'>
-                    <img/>
-                </div>
-                <div class='info'>
-                </div>
-                <div class='buttons'>
-                    <a href=''></a>
-                    <a href=''></a>
-                    <a href=''></a>
-                </div>
-            </section>
-        )
-
-    }
+const Profile = () => {
+    return (
+        <section>
+            <div class='image-container'>
+                <img/>
+            </div>
+            <div class='info'>
+            </div>
+            <div class='buttons'>
+                <a href=''></a>
+                <a href=''></a>
+                <a href=''></a>
+            </div>
+        </section>
+    )
 }
 
 export default Profile
--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,23 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Users from './User'
-class UsersList extends Component{
+const UsersList = ({foundUsers, setChat, createNewChat, history}) => {
 
-    createChat(userId){
+    const createChat = (userId) => {
         fetch(`http://localhost:8080/api/chat/auth/create?userId=${userId}`)        
     }
 
-    render(){
-        return (
+    return (
         <div className='users'>
-           {this.props.foundUsers.map(user =>{
-                return(
-                    <Users user={user} history={this.props.history} setChat={this.props.setChat} createNewChat={this.props.createNewChat} key={user.id} />
-                )
-            })}
+            {foundUsers.map(user =>
+                <Users user={user} history={history} setChat={setChat} createNewChat={createNewChat} key={user.id} />
+            )}
         </div>
-        )
-
-    }
+    )
 }
 
 export default UsersList
