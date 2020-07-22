0a7ab684a9892a77c9a1f1c67f2a20fa46561b05
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-07-22 12:10:05 +0300

    Menu - rotation from ref to state + button text + function swap

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,21 +2,23 @@ import React, { useRef, useState } from 'react';
 import {Link, useHistory} from 'react-router-dom'
 
 const Menu = ({chatClass, setChatsClass, logout, appType}) => {
+    const [menuClass, setMenuClass] = useState('menu-container hidden')
+    const [rotate, setRotate] = useState({})
     const history = useHistory()
     const nav = useRef()
-    const rotationDeg = useRef(0)
+    const [rotationStyle, setrotationStyle] = useState(0)
     const isRotating = useRef(false)
     const rotatingSign = useRef()
-    const [menuClass, setMenuClass] = useState('menu-container hidden')
+    const minRotation = -60
 
     const toggleMenu = () => {
         setMenuClass(menuClass == 'menu-container hidden' ? 'menu-container' : 'menu-container hidden')    
     }
 
-    const toggleHeader = () => {
+    const toggleHeader = () => { 
         setChatsClass(chatClass == 'chat-users hidden' ? 'chat-users' : 'chat-users hidden')    
     }
-
+    
     const logoutAndRedirect = () => {
         logout()
         history.push('/login')
@@ -27,9 +29,9 @@ const Menu = ({chatClass, setChatsClass, logout, appType}) => {
         if(!isRotating.current || sign != rotatingSign.current){
             rotatingSign.current = sign
             const newDeg = 20 * sign
-            const max = -rotationDeg.current
-            const min = -60 - rotationDeg.current  
-            smoothRotate(Math.min(Math.max(newDeg, min), max), 650, rotationDeg.current)
+            const max = -rotationStyle
+            const min = -60 - rotationStyle  
+            smoothRotate(Math.min(Math.max(newDeg, min), max), 650, rotationStyle)
         }
     }
 
@@ -42,13 +44,9 @@ const Menu = ({chatClass, setChatsClass, logout, appType}) => {
             const timeElapsed = currentTime - startTime;
 
             const amount = linearTween(timeElapsed, startPos, deg, durration);
-
-            nav.current.style.msTransform = `rotate(${amount}deg)`
-            nav.current.style.webkitTransform = `rotate(${amount}deg)`
-            nav.current.style.MozTransform = `rotate(${amount}deg)`
-            nav.current.style.OTransform = `rotate(${amount}deg)`
-            nav.current.style.transform = `rotate(${amount}deg)`
-            rotationDeg.current = amount
+            const rotateString = `rotate(${amount}deg)`
+            setRotate({WebkitTransform: rotateString, MozTransform: rotateString, OTransform: rotateString, transform: rotateString})
+            setrotationStyle(amount)
             
             if(durration / 2 < timeElapsed ) isRotating.current = false
             if(timeElapsed < durration) return requestAnimationFrame(rotate);
@@ -60,15 +58,15 @@ const Menu = ({chatClass, setChatsClass, logout, appType}) => {
     }
 
     const showRestMenu = () => {
-        const deg = -60 - rotationDeg.current
+        const deg = -60 - rotationStyle
         const transition = deg / -20 * 650
-        smoothRotate(deg, transition, rotationDeg.current)
+        smoothRotate(deg, transition, rotationStyle)
     }
 
     const showChatMenu = () => {
-        const deg = -rotationDeg.current
+        const deg = -rotationStyle
         const transition = deg / 20 * 650
-        smoothRotate(deg, transition, rotationDeg.current)
+        smoothRotate(deg, transition, rotationStyle)
     }
 
     return (
@@ -77,14 +75,14 @@ const Menu = ({chatClass, setChatsClass, logout, appType}) => {
             <div>
                 <div className='circle-nav' onWheel={rotateNav} id='cn-wrapper'>
                     <button onClick={toggleMenu} className='circle-btn' tabIndex="-1">-</button>
-                    <ul ref={nav}>
+                    <ul ref={nav} style={rotate}>
                         <li><button tabIndex="-1"><i className='fas fa-user'></i></button></li>
-                        <li><button onClick={toggleHeader} tabIndex="-1"><i className='fas fa-comments'></i></button></li>
+                        <li><button onClick={toggleHeader} tabIndex="-1"><i className={chatClass == 'chat-users hidden' ? 'far fa-eye' : 'far fa-eye-slash'}></i></button></li>
                         <li><Link to='/searchChat' tabIndex="-1"><i className='fas fa-search'></i></Link></li>
                         <li><Link to='/searchUsers' tabIndex="-1"><i className='fas fa-user-plus'></i></Link></li>
                         {appType == 'restaurant' && 
                             <>
-                                <li><button onClick={showRestMenu} tabIndex="-1">R</button></li>
+                                <li><button onClick={rotationStyle <= minRotation ? showChatMenu : showRestMenu} tabIndex="-1">{rotationStyle <= minRotation ? 'C' : 'R'}</button></li>
                             </>
                         }                        
                         <li><button onClick={logoutAndRedirect} tabIndex="-1"><i className='fas fa-sign-out-alt'></i></button></li>
