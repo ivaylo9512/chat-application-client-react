ae407233731015f42b2496d8b684bb5b6c0408e2
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-07-31 23:53:00 +0300

    SearchUsers fetch to useRequest

--- src/components/SearchUsers.js
+++ src/components/SearchUsers.js
@@ -1,40 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import {useInput} from '../hooks/useInput'
+import { useRequest } from '../hooks/useRequest';
 
 const SearchUsers = ({setFoundUsers, searchClass}) => {  
     const [name, nameInput] = useInput({type: 'text', placeholder:'search users'})
-    const [url, setUrl] = useState(undefined)
-    const [error, setError] = useState(undefined)
+    const [users, fetchRequest] = useRequest({initialValue: [], isAuth: true, callback: setFoundUsers})
 
-    useEffect(() => {
-        let isCurrent = true;
-        if(url){
-            async function searchUsers(){
-                const response = await fetch(url, {
-                    headers: {
-                        'Authorization': localStorage.getItem('Authorization')
-                    }
-                })
-                const data = await response.text()
-                if(isCurrent){
-                    if(response.ok){
-                        setFoundUsers(JSON.parse(data))
-                    }else{
-                        setError(data)
-                    }
-                }
-            }
-            searchUsers()
-        }    
-        return () => {
-            isCurrent = false
-        }
-    }, [url, setFoundUsers])
-    
-    
     return (
         <div className={searchClass}>
-            <form onSubmit={() => setUrl(`http://${localStorage.getItem('BaseUrl')}/api/users/auth/searchForUsers/${name}`) }>
+            <form onSubmit={() => fetchRequest({url:`http://${localStorage.getItem('BaseUrl')}/api/users/auth/searchForUsers/${name}`})}>
                 {nameInput}
                 <button type='submit'><i className='fas fa-search'></i></button>
             </form>
