0af1b3ff921fd29242352c20ca2956d358066f26
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-08-08 21:16:44 +0300

    filtered chats added

--- src/components/Logged.js
+++ src/components/Logged.js
@@ -7,12 +7,13 @@ import useRequest from '../hooks/useRequest'
 const Logged = ({user, appType}) => {
     const [headerType, setHeaderType] = useState('chats')
     const [chat, setChat] = useState(undefined)
+    const [filteredChats, setfilteredChats] = useState([])
     const [order, setOrder] = useState(undefined)
     const [headerClass, setHeaderClass] = useState('header-scroll hidden')
     const [searchClass, setSearchClass] = useState('form-container')
     const isLongPolling = useRef(localStorage.getItem('LongPolling'))
 
-    const [chats, fetchChats, ordersError, setChats] = useRequest({initialUrl:`http://${localStorage.getItem('BaseUrl')}/api/chat/auth/getChats?pageSize=3`, initialValue:[], isAuth: true, fetchOnMount:!isLongPolling})
+    const [chats, fetchChats, ordersError, setChats] = useRequest({initialUrl:`http://${localStorage.getItem('BaseUrl')}/api/chat/auth/getChats?pageSize=3`, initialValue:[], isAuth: true, fetchOnMount:!isLongPolling, callback:setfilteredChats})
     const [orders, fetchOrders, chatsError, setOrders] = useRequest({initialUrl: `http://${localStorage.getItem('BaseUrl')}/api/orders/auth/getOrders`, initialValue:[], isAuth: true})
 
     const setData = useCallback((data) => {
@@ -24,18 +25,15 @@ const Logged = ({user, appType}) => {
 
     const searchChats = (name) => {
         name = name.toUpperCase()
-        const filteredChats = chats.filter(chat => { 
+        setfilteredChats(chats.filter(chat => { 
             const firstName = chat.user.firstName.toUpperCase()
             const lastName = chat.user.lastName.toUpperCase()
             if(firstName.startsWith(name) || lastName.startsWith(name) ||(`${firstName} ${lastName}`).startsWith(name)){
                 return chat
             }
             return null;
-        })
-        if(headerClass.includes('header-scroll')){
-            setHeaderType('chats')
-            setHeaderClass('header-scroll')
-        }
+        }))
+        setHeaderType('chats')
     }
 
     const setHeader = useCallback(() => {
@@ -54,7 +52,7 @@ const Logged = ({user, appType}) => {
 
     return(
         <div className='content-container'>
-            <HeaderScroll setOrder={setOrder} setChat={setChat} headerClass={headerClass} headerType={headerType} currentList={headerType == 'chats' ? chats : orders}/>
+            <HeaderScroll setOrder={setOrder} setChat={setChat} headerClass={headerClass} headerType={headerType} currentList={headerType == 'chats' ? filteredChats : orders}/>
             <div className='content'>
                 <Menu headerClass={headerClass} searchClass={searchClass} headerType={headerType} setHeader={setHeader} setHeaderClass={setHeaderClass} setSearchClass={setSearchClass} appType={appType}/>
                 <Main searchChats={searchChats} searchClass={searchClass} chat={chat} order={order}/>
