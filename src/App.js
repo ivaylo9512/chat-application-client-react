394ae01a49536d818e8c1d3a5be081588871fbe6
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-08-10 22:48:48 +0300

    case added for getting logged user in app + login url change

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Register from './components/Register'
 import Logged from './components/Logged'
 import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom'
 import ErrorBoundary from './components/ErrorBoundary'
+import { useRequest } from './hooks/useRequest';
 
 const App = () => {
     const [user, setUser] = useState(undefined)
     const [auth, setAuth, removeAuth] = useLocalStorage('Authorization', null)
     const [appType, setAppType] = useLocalStorage('appType', null)
+    const [user, fetchUser, userError, setUser] = useRequest({isAuth:true})
 
     const logout = () => {
         setUser(null)
@@ -33,10 +35,16 @@ const App = () => {
     }
 
     useEffect(() => {
-        localStorage.setItem('Polling', 'WebSocket' in window && window.WebSocket.CLOSING === 2 
+        localStorage.setItem('LongPolling', 'WebSocket' in window && window.WebSocket.CLOSING === 2 
             ? '' 
-            : 'polling'
+            : 'polling/'
         )
+    },[])
+
+    useEffect(() => {
+        if(auth){
+            fetchUser({url:`http://${localStorage.getItem('BaseUrl')}/api/users/${localStorage.getItem('LongPolling')}auth/getLoggedUser`})
+        }
     },[])
 
     return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -6,15 +6,14 @@ import { useRequest } from '../hooks/useRequest'
 const Login = ({setUser, setAuth, resetAppType}) => {
     const [username, usernameInput] = useInput({type: 'text', placeholder:'username'})
     const [password, passwordInput] = useInput({type: 'password', placeholder:'password'})
-    const [error, setError] = useState()
-    const loginUrl = useRef(localStorage.getItem('LongPolling') ? `http://${localStorage.getItem('BaseUrl')}/api/polling/login` : `http://${localStorage.getItem('BaseUrl')}/api/users/login`)
+    const [error] = useState()
 
     const onSuccessfulLogin  = useCallback((data, headers) => {
         setUser(data)
         setAuth(headers.get('Authorization'))
     },[setAuth, setUser])
 
-    const [userInfo, fetchLogin] = useRequest({initialUrl:`http://${localStorage.getItem('BaseUrl')}/api/users/login`, callback: onSuccessfulLogin, fetchOnMount: false, method: 'post', shouldThrow: false})
+    const [userInfo, fetchLogin] = useRequest({initialUrl:`http://${localStorage.getItem('BaseUrl')}/api/users/${localStorage.getItem('LongPolling')}login`, callback: onSuccessfulLogin, fetchOnMount: false, method: 'post', shouldThrow: false})
 
     const login = (e) => {
         e.preventDefault();
