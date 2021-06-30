1f7e271646a6c195655b927f4616e8f676c2f8e7
ivaylo <ivayloaleksandrov@yahoo.com>
2021-06-30 20:47:47 +0300

    small change name added

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-export const useInput = ({type, placeholder, validationRules, equalsValue, equalsName}) => {
+export const useInput = ({type, placeholder, name, validationRules, equalsValue, equalsName}) => {
     const [value, setValue] = useState('');
-    const inputElement = useRef()
+    const inputElement = useRef();
 
     const onChange = ({target, target: {value}}) => {
-        setValue(value)
-        validate(target, value)
-    }
+        setValue(value);
+        validate(target, value);
+    };
 
     const validate = (input, value) => {
         if(equalsValue){
@@ -16,15 +16,15 @@ export const useInput = ({type, placeholder, validationRules, equalsValue, equal
                 : ''
             )
         }
-    }
+    };
 
     useEffect(() =>{
         if(equalsValue){
-            validate(inputElement.current, value)    
+            validate(inputElement.current, value);    
         }
-    },[equalsValue])
+    },[equalsValue]);
 
-    const input = <input value={value} ref={inputElement} onChange={onChange} {...validationRules} type={type} placeholder={placeholder} />
+    const input = <input value={value} ref={inputElement} name={name} onChange={onChange} {...validationRules} type={type} placeholder={placeholder} />
     
     return[
         value,
