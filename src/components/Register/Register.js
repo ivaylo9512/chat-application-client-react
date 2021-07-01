b84acd7489d9947804b58803bbf75b902be9a0a6
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-01 23:31:23 +0300

    small fix error moved to useRequest + lines

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,40 +4,74 @@ import { useInput } from '../../hooks/useInput';
 import { useRequest } from '../../hooks/useRequest';
 
 const Register = ({setAuthUser}) => {
-    const [userInfo, setUserInfo] = useState(undefined)
-    const [apiError, setApiError] = useState('')
     const [pageIndex, setPageIndex] = useState(0)
-    const [registeredUser, fetchRegister] = useRequest({initialUrl: `${localStorage.getItem('BaseUrl')}/api/users/${localStorage.getItem('LongPolling')}/register`, shouldThrow: false, callback: setAuthUser, type: 'post'})
+    const [registeredUser, fetchRegister, error] = useRequest({
+        initialUrl: `${localStorage.getItem('BaseUrl')}/api/users/${localStorage.getItem('LongPolling')}/register`, 
+        shouldThrow: false, 
+        callback: 
+        setAuthUser, 
+        type: 'post'
+    })
 
-    const [username, usernameInput] = useInput({type: 'text', placeholder: 'username', validationRules: {
-        minLength: 6,
-        maxLength: 15,
-        required: true
-    }}) 
-    const [password, passwordInput] = useInput({type: 'password', placeholder: 'password', validationRules:{
-        minLength: 7,
-        maxLength: 25,
-        required: true
-    }}) 
-    const [repeat, repeatInput] = useInput({type: 'password', placeholder: 'repeat', validationRules:{
-        required: true,
-    },equalsValue: password, equalsName: 'Passwords'}) 
-    const [firstName, firstNameInput] = useInput({type: 'text', placeholder: 'first name', validationRules: {
-        required: true
-    }}) 
-    const [lastName, lastNameInput] = useInput({type: 'text', placeholder: 'last name', validationRules: {
-        required: true
-    }}) 
-    const [country, countryInput] = useInput({type: 'text', placeholder: 'country', validationRules: {
-        required: true
-    }}) 
-    const [age, ageInput] = useInput({type: 'number', placeholder: 'age', validationRules: {
-        required: true
-    }}) 
+    const [username, usernameInput] = useInput({
+        type: 'text', 
+        placeholder: 'username', 
+        validationRules: {
+            minLength: 6,
+            maxLength: 15,
+            required: true
+        }
+    }) 
+    const [password, passwordInput] = useInput({
+        type: 'password', 
+        placeholder: 'password', 
+        validationRules:{
+            minLength: 7,
+            maxLength: 25,
+            required: true
+        }
+    }) 
+    const [repeat, repeatInput] = useInput({
+        type: 'password', 
+        placeholder: 'repeat', 
+        validationRules: {
+            required: true,
+        },
+        equalsValue: password, 
+        equalsName: 'Passwords'
+    }) 
+    const [firstName, firstNameInput] = useInput({
+        type: 'text', 
+        placeholder: 'first name', 
+        validationRules: {
+            required: true
+        }
+    }) 
+    const [lastName, lastNameInput] = useInput({
+        type: 'text', 
+        placeholder: 'last name', 
+        validationRules: {
+            required: true}
+        }) 
+    const [country, countryInput] = useInput({
+        type: 'text', 
+        placeholder: 'country', 
+        validationRules: {
+            required: true
+        }
+    }) 
+    const [age, ageInput] = useInput({
+        type: 'number', 
+        placeholder: 'age', 
+        validationRules: {
+            required: true
+        }
+    }) 
 
     const register = (e) => {
         e.preventDefault()
-        fetchRegister({body:{username, password, repeat, firstName, lastName, country, age}})
+        fetchRegister({
+            body:{username, password, repeat, firstName, lastName, country, age}})
     }
 
     const setPage = (e, page) => {
@@ -54,7 +88,7 @@ const Register = ({setAuthUser}) => {
                     {repeatInput}
                     <button type='submit'>next</button>
                     <span>Already have an account?<Link to='/login'> Log in.</Link></span>
-                    <span>{apiError}</span>
+                    <span>{error}</span>
                 </form> :
                 <form onSubmit={register}>
                     {firstNameInput}
