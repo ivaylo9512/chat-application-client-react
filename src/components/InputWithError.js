dca2361ef4ac875f2d758bc78f683d3a8a330cb8
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-01 23:46:25 +0300

    InputWithError added

---
+++ src/components/InputWithError.js
@@ -0,0 +1,16 @@
+
+const InputWithError = ({error, classname = '', input}) => {
+
+    return(
+        <div className={(error ? 'error ' : '') + classname}>
+            {input}
+            {error && 
+                <div>
+                    {error}
+                </div>
+            }
+        </div>
+    )
+}
+
+export default InputWithError
