3e7e1d3363a814085d1dc06794ccb2f4db1faed4
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-09 23:43:38 +0300

    HeaderScroll padding style change + fetch more chast on scroll + css

--- src/components/HeaderScroll/HeaderScroll.js
+++ src/components/HeaderScroll/HeaderScroll.js
@@ -2,16 +2,18 @@ import React, { useEffect, useState, useRef } from 'react';
 import smoothscroll from 'smoothscroll-polyfill';
 import './HeaderScroll.css'
 import { getHeaderVisibility } from '../../app/slices/stylesSlice';
-import { useSelector } from 'react-redux';
-import { getChatsQuery } from '../../app/slices/chatsSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { getChatsQuery, getChatsData, getChatsState, chatsRequest } from '../../app/slices/chatsSlice';
 import ChatList from '../ChatList/ChatList';
 
 const HeaderScroll = () => {
     const listContainer = useRef()
     const scrollContainer = useRef()
-    const [listPadding, setListPadding] = useState({})
+    const [paddingBottom, setPaddingBottom] = useState(0)
     const query = useSelector(getChatsQuery)
     const isHidden = useSelector(getHeaderVisibility) 
+    const { isLoading, data: { isLastPage } } = useSelector(getChatsState);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         hideScrollBar()
@@ -30,16 +32,26 @@ const HeaderScroll = () => {
 
         const barHeight = containerHeight - height
     
-        setListPadding({paddingBottom:`${barHeight}px`})
+        setPaddingBottom(`${barHeight}px`)
     }
 
-    const scroll = (e) => {
-        e.currentTarget.scroll({left: e.currentTarget.scrollLeft + e.deltaY * 4 , top: e.currentTarget.scrollTop, behavior: 'smooth'})
+    const scroll = ({currentTarget, deltaY}) => {
+        const {scrollLeft, scrollTop} = currentTarget
+        currentTarget.scroll({left: scrollLeft + deltaY * 4 , top: scrollTop, behavior: 'smooth'})
+
+        fetchMoreChats(currentTarget);
+        console.log(scrollLeft + currentTarget.clientWidth == currentTarget.scrollWidth);
+    }
+
+    const fetchMoreChats = ({scrollLeft, clientWidth, scrollWidth}) => {
+        if(scrollLeft + clientWidth == scrollWidth && !isLastPage && !isLoading){
+            dispatch(chatsRequest({...query}))
+        }
     }
 
     return (
         <div className={`header-scroll${isHidden ? ' hidden' : ''}`} ref={scrollContainer}>
-            <div ref={listContainer} style={listPadding} onWheel={scroll}>
+            <div ref={listContainer} style={{paddingBottom}} onWheel={scroll}>
                 <ChatList />
             </div>
         </div>
