d575318116c7a0980d03871061386a49bd284055
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-09 23:44:06 +0300

    chas split array removed + change for  pageable

--- src/app/sagas/chats.js
+++ src/app/sagas/chats.js
@@ -2,53 +2,43 @@ import { BASE_URL } from '../../constants';
 import { getChatsData, onChatsComplete, onChatsError } from '../slices/chatsSlice';
 import { takeLatest, select, put } from 'redux-saga/effects';
 
-export default takeLatest('chats/getChats', getChats);
+export default takeLatest('chats/chatsRequest', getChats);
 
 function* getChats({payload: query}){
-    const { lastUpdatedAt, lastId, takeAmount } =  getData(query, yield select(getChatsData));
-    const response = yield fetch(`${BASE_URL}/api/chats/auth/findChats/${takeAmount}${lastUpdatedAt}/${lastId}`,{
+    const { lastUpdatedAt, lastId, take } =  getData(query, yield select(getChatsData));
+    const lastParam = lastUpdatedAt ? `${lastUpdatedAt}/${lastId}` : ''
+    
+    const response = yield fetch(`${BASE_URL}/api/chats/auth/findChats/${take}/${lastParam}`, {
         headers:{
-            Authorization: `Bearer ${localStorage.getItem('Authorization')}`
+            Authorization: localStorage.getItem('Authorization')
         }
     });
 
     if(response.ok){
-        const data = yield response.json(); 
+        const pageable = yield response.json(); 
+        pageable.lastChat = pageable.data[pageable.data.length - 1]; 
+        pageable.isLastPage = pageable.pages < 2;
         
-        data.length = data.chats.length;
-        data.lastChat = data.chats[data.length];
-        data.chats = splitArray(data.chats, query.take);
-
         yield put(onChatsComplete({
-            data,
+            pageable,
             query
         }))
     }else{
-        yield put (onChatsError(response.text()));
+        yield put(onChatsError(yield response.text()));
     }
 
-}
-
-const splitArray = (chats, take) => {
-    return chats.reduce((result, chat, i) =>  {
-        const page = Math.floor(i / take);
-        result[page] = result[page] ? (result[page].push(chat), result[page]) : [chat];
-
-        return result;
-    },[])
 }
 
 const getData = (query, data) => {
     let lastId = 0;
     let lastUpdatedAt;
     let lastChat = data.lastChat;
-    const takeAmount = query.take * query.pages;
     
     if(lastChat){
         lastId = lastChat.id;
         lastUpdatedAt = lastChat.updatedAt;
     }
 
-    return {...query, takeAmount, lastUpdatedAt, lastId}
+    return {...query, lastUpdatedAt, lastId}
 }
 
