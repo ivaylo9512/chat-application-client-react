ad44b102f086a37ffed70d79164bbeabecea24ca
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-10 22:52:47 +0300

    ChatUser from css to styledComponent

--- src/components/ChatUser/ChatUser.js
+++ src/components/ChatUser/ChatUser.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
 import { setCurrentChat } from '../../app/slices/chatsSlice';
-import './ChatUser.css';
+import { BASE_URL } from '../../constants';
+import { faInfo } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ChatContainer, Chat, ImageContainer, Info, LastMsg, InfoButton } from './ChatUserStyles';
 
 const ChatUser = ({chat}) => {
     const dispatch = useDispatch();
     const history = useHistory();
+    const [displayInfo, setDisplayInfo] = useState(false);
 
     const setChat = () => {
         dispatch(setCurrentChat(chat));
@@ -14,19 +18,22 @@ const ChatUser = ({chat}) => {
     }
 
     return (
-        <div className='chat-container' key={chat.id}> 
-            <button onClick={setChat} className='chat'>
+        <ChatContainer> 
+            <Chat onClick={setChat}>
                 <div>
-                    <div className='image'>
-                        <img alt='profile' src={chat.secondUser.profilePicture}/>
-                    </div>
-                    <div className='info'>
+                    <ImageContainer>
+                        <img alt='profile image' src={`${BASE_URL}/images/get/${chat.secondUser.profileImage}`}/>
+                    </ImageContainer>
+                    <Info displayInfo={displayInfo}>
                         <b>{chat.secondUser.username}</b>
-                        <span></span>    
-                    </div>
+                        <LastMsg>{chat.lastMessage}</LastMsg>    
+                    </Info>
+                    <InfoButton onClick={() => setDisplayInfo(!displayInfo)}>
+                        <FontAwesomeIcon icon={faInfo} />
+                    </InfoButton>
                 </div>
-            </button>
-        </div>
+            </Chat>
+        </ChatContainer>
     )
 }
 
---
+++ src/components/ChatUser/ChatUserStyles.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+
+export const ChatContainer = styled.div`
+    height: 10.5vh;
+    flex: 0 0 23vw;
+    padding: 0 0.5%;
+    background: white;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    margin-right: 0.5vw;
+`
+export const Chat = styled.div`
+    padding: 0 5%;
+    border-radius: 0.5vw;
+    width: 95%;
+    height: 75%;
+    position: relative;
+    background: rgb(24,27, 33);
+    text-decoration: none;
+    box-shadow: 0px 0px 0.15vw 0.15vw rgb(0 0 0);
+
+    > div {
+        height: 100%;
+        width: 100%;
+        align-items: center;
+        display: flex;
+    }
+`
+export const ImageContainer = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 9.5vh;
+    width: 9.5vh;
+    border-radius: 50%;
+    background: rgb(24,27, 33);
+    box-shadow: 0px 0px 0.5vw 0.12vw rgb(0 0 0 / 80%);
+
+    img{
+        border-radius: 50%;
+        width: 5vh;
+        height: 5vh;
+        filter: drop-shadow(0px 1px 3px rgb(0 0 0));
+    }
+`
+
+export const LastMsg = styled.span`
+    color: #dbdada;
+    text-decoration: overline;
+    display: none;
+`
+
+export const Info = styled.div`
+    flex: 1;
+    font-size: 0.9vw;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    b{
+        color: white;
+        vertical-align: bottom;
+        text-align: center;
+    }
+
+    ${LastMsg} {
+        display: ${props => props.displayInfo ? 'block' : 'none'};
+      }
+`
+
+export const InfoButton = styled.button`
+    height: 2.5vh;
+    width: 2.5vh;
+    font-size: 1.2vh;
+    border-radius: 50%;
+`
