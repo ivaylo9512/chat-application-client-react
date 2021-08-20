796885120c7c67dbf1de603f248e0f989a21947e
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-20 22:31:56 +0300

    Logout added

---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,19 @@
+
+import { onLogout } from 'app/slices/authenticateSlice';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { Redirect } from "react-router-dom";
+const Logout = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+            localStorage.removeItem('Authorization');
+            localStorage.removeItem('user');
+            dispatch(onLogout());
+    }, [])
+
+    return(
+        <Redirect to="/" />
+    )
+}
+export default Logout
