b3b7c8374f4480292d40913f06b26d6ca0f928ee
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-21 19:19:10 +0300

    ChatList unit tests added

---
+++ src/components/ChatList/tests/unit.test.js
@@ -0,0 +1,54 @@
+import * as redux from 'react-redux';
+import { shallow } from 'enzyme';
+import ChatList from 'components/ChatList/ChatList';
+import Chat from 'components/Chat/Chat';
+import LoadingIndicator from 'components/LoadingIndicator/LoadingIndicator';
+import { chatsRequest } from 'app/slices/chatsSlice';
+
+const chats = [ { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 } ];
+
+describe('ChatList unit tests', () => {
+    let dispatchMock = jest.fn();
+
+    beforeEach(() => {
+        jest.spyOn(redux, 'useDispatch').mockReturnValue(dispatchMock);
+    })
+    
+    const createWrapper = (state) => {
+        jest.spyOn(redux, 'useSelector').mockReturnValue(state);
+
+        return shallow(<ChatList />)
+    }
+
+    it('should render Chats', () => {
+        const wrapper = createWrapper({ isLoading: false, data: { chats, isLastPage: true }});
+
+        const foundChats = wrapper.find(Chat);
+        expect(foundChats.length).toBe(4);
+
+        expect(foundChats.at(0).prop('chat')).toBe(chats[0])
+        expect(foundChats.at(1).prop('chat')).toBe(chats[1])
+
+        expect(foundChats.at(0).key()).toBe('5')
+        expect(foundChats.at(1).key()).toBe('6')
+    })
+
+    it('should render LoadingIndicator when loading', () => {
+        const wrapper = createWrapper({ isLoading: true, data: { chats: [], isLastPage: false }});
+
+        expect(wrapper.find(LoadingIndicator).length).toBe(1);
+    })
+
+    it('should render info when empty chats array and not loading ', () => {
+        const wrapper = createWrapper({ isLoading: false, data: { chats: [], isLastPage: false }});
+
+        expect(wrapper.findByTestid('info').length).toBe(1);
+    })
+
+    it('should render more button when lastPage is false', () => {
+        const wrapper = createWrapper({ isLoading: false, data: { chats, isLastPage: false }});
+
+        expect(wrapper.findByTestid('more').length).toBe(1);
+    })
+
+})
