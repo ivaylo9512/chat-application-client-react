3116500e840a3b653ae5d5a77c56a3fc55f5bd84
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-22 19:10:42 +0300

    UserChat snapshot tests added

---
+++ src/components/UserChat/tests/snapshot.test.js
@@ -0,0 +1,22 @@
+import * as redux from 'react-redux';
+import { shallow } from 'enzyme';
+import UserChat from 'components/UserChat/UserChat';
+
+describe('UserChat snapshot tests', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        jest.spyOn(redux, 'useDispatch').mockReturnValue(jest.fn());
+
+        wrapper = shallow(<UserChat userChat={{id: 1, createdAt:'2021-08-22', secondUser: { profileImage: 'image.png', firstName: 'firstname', lastName: 'lastName' }}}/>)
+    })
+
+    it('should match snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    it('should match snapshot with toggled info', () => {
+        wrapper.findByTestid('toggleInfo').simulate('click');
+        expect(wrapper).toMatchSnapshot();
+    })
+})
