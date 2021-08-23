e6e656e58f70c57eea1837aae31b47b520cc7f33
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-23 04:34:08 +0300

    RequestStyle + RequestListStyle + UsersList page removed

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,18 +1,17 @@
 import User from 'components/User/User'
 import { useSelector } from 'react-redux';
-import { getCurrentUsers, getCurrentPage } from 'app/slices/usersSlice';
+import { getCurrentUsers } from 'app/slices/usersSlice';
 import { Container } from './UsersListStyle';
 
 const UsersList = () => {
     const users = useSelector(getCurrentUsers);
-    const page = useSelector(getCurrentPage);
 
     return (
         <Container>
             {users && (users.length == 0 
                 ? <span data-testid='info'><b>No users found with this search.</b></span>
                 : users.map(user =>
-                    <User user={user} key={user.id} page={page} />
+                    <User user={user} key={user.id} />
             ))}
         </Container>
     )
