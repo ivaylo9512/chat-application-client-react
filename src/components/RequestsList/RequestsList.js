6b0aab92aeeee43d8a060e18b217a40c268dd0ce
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-23 04:00:14 +0300

    RequestList added

---
+++ src/components/RequestsList/RequestsList.js
@@ -0,0 +1,29 @@
+import { useSelector, useDispatch } from "react-redux"
+import { getCurrentAllRequests, resetRequestsState, getRequests, getAllRequestsQuery } from "app/slices/allRequestsSlice"
+import Request from "components/Request/Request";
+import { Container } from './RequestsListStyle';
+import { useEffect } from "react";
+
+const RequestsList = () => {
+    const requests = useSelector(getCurrentAllRequests);
+    const query = useSelector(getAllRequestsQuery);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getRequests({ ...query, pages: 1 }));
+
+        return () => dispatch(resetRequestsState());
+    }, [])
+
+    return(
+        <Container>
+            {requests && (requests.length == 0
+                ? <span data-testid='info'><b>No requests found.</b></span> 
+                : requests.map(request => 
+                    <Request key={request.id} request={request} />)
+            )}
+        </Container>
+    )
+
+}
+export default RequestsList
