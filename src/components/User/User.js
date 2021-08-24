b59475e44a0f5a1dcc23bbc9c24cc53d56318841
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-25 01:54:04 +0300

    User message changed + styled elements added + info toggle

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -6,50 +6,57 @@ import LoadingIndicator from 'components/LoadingIndicator/LoadingIndicator';
 import { memo } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import requests from 'app/sagas/requests';
+import { Container, Image, Info, ButtonsContainer, Button, InfoContainer } from 'components/User/UserStyle';
+import { IMAGE_URL } from 'appConstants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInfo, faForward } from '@fortawesome/free-solid-svg-icons'
 
-const User = memo(({user}) => {
+const User = memo(({user: { id, profileImage, firstName, lastName, chatWithUser, createdAt, requestState }}) => {
     const history = useHistory();
     const dispatch = useDispatch();
-    const request = useSelector(state => state.requests.data[user.id]);
-    const [message, setMessage] = useState(user.chatWithUser 
-        ? 'open' 
-        : user.requestState
-    );
+    const request = useSelector(state => state.requests.data[id]);
+    const [isInfoVisible, setIsInfoVisible] = useState(false);
+    const [message, setMessage] = useState(requestState);
 
     const setChat = () => {
         history.push('/chat');
-        dispatch(setCurrentChat(user.chatWithUser));
+        dispatch(setCurrentChat(chatWithUser));
     }
 
     const manageRequest = () => {
         if(message != 'pending'){
-            dispatch(sendRequest(user.id));
+            dispatch(sendRequest(id));
         }
     }
 
     useEffect(() => {
         if(request && !request.isLoading){
-            setMessage(request.chatWithUser ? 'open' : request.state);
+            setMessage(request.state);
         }
     }, [request])
 
     return (
-        <div>
-            <div className='image'>
-                <img alt='profile' src={user.profilePicture}/>
-            </div>
-            <div className='info'>
-                <b>{`${user.firstName} ${user.lastName}`}</b>
-                <span></span>    
-            </div>
-            <button onClick={message == 'open' ? setChat : manageRequest}>
-                {request?.isLoading
-                    ? <LoadingIndicator />
-                    : message
-                }
-            </button>
-        </div>
+        <Container>
+            <Image>
+                <img src={`${IMAGE_URL}/${profileImage}`}/>
+            </Image>
+            <InfoContainer isInfoVisible={isInfoVisible}>
+                <span><b>{`${firstName} ${lastName}`}</b></span>
+                <Info isInfoVisible={isInfoVisible}>
+                    <span><b>{`${firstName} ${lastName}`}</b></span>
+                    <span><b>{createdAt}</b></span>
+                </Info>
+            </InfoContainer>
+            <ButtonsContainer>
+                <Button data-testid='toggleInfo' onClick={() => setIsInfoVisible(!isInfoVisible)}><FontAwesomeIcon icon={faInfo}/></Button>
+                <Button data-testid='action' onClick={message == 'open' ? setChat : manageRequest}>
+                    {request?.isLoading
+                        ? <LoadingIndicator />
+                        : message
+                    }
+                </Button>
+            </ButtonsContainer>
+        </Container>
     )
 })
 
