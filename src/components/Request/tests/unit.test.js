6e851d0c3a2fc68e6712a891a3d17ee873651b4b
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-25 07:52:10 +0300

    unit tests removed from Request/User for moved requests button login + test added for Error

--- src/components/Request/tests/unit.test.js
+++ src/components/Request/tests/unit.test.js
@@ -1,70 +1,39 @@
 import * as redux from 'react-redux';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Request from 'components/Request/Request';
-import LoadingIndicator from 'components/LoadingIndicator/LoadingIndicator';
-import { setCurrentChat } from 'app/slices/chatsSlice';
-import { acceptRequest } from 'app/slices/requestsSlice';
 import { Info } from 'components/Request/RequestStyle';
 import 'jest-styled-components';
-import reactRouterDom from 'react-router-dom';
+import { Error } from 'components/Request/RequestStyle';
+import { act } from 'react-dom/test-utils';
 
-jest.mock('react-router-dom')
+jest.mock('components/RequestButtons/RequestButtons', () => () => <div></div>)
 
-const chatWithUser = {id: 1, lastMessage: 'last message', secondUser: { firstName: 'first', lastName: 'last', profileImage: 'image1.png'} };
 describe('Request unit tests', () => {
-    let dispatchMock = jest.fn();
-    let pushMock = jest.fn();
-
-    beforeEach(() => {
-        jest.spyOn(redux, 'useDispatch').mockReturnValue(dispatchMock);
-        reactRouterDom.useHistory = jest.fn().mockReturnValue({push: pushMock});
-    })
-    
     const createWrapper = (state) => {
         jest.spyOn(redux, 'useSelector').mockReturnValue(state);
 
-        return shallow(
+        return mount(
             <Request request={{ id: 5, createdAt: '2021-08-23', sender: { firstName: 'firstname', lastName: 'lastName', profileImage: 'image.png' }}}/>
         )
     }
 
-    it('should call dispatch with setCurrentChat when request has chatWithUser', () => {
-        const wrapper = createWrapper({ isLoading: false, chatWithUser});
-        wrapper.findByTestid('action').simulate('click');
-
-        expect(dispatchMock).toHaveBeenCalledWith(setCurrentChat(chatWithUser));
-    })
-
-    it('should call dispatch with acceptRequest when there is no request', () => {
-        const wrapper = createWrapper(undefined);
-        wrapper.findByTestid('action').simulate('click');
-
-        expect(dispatchMock).toHaveBeenCalledWith(acceptRequest(5));
-    })
-
-    it('should render LoadingIndicator when request isLoading is true', () => {
-        const wrapper = createWrapper({ isLoading: true });
-
-        expect(wrapper.find(LoadingIndicator).exists()).toBe(true);
-    })
-
     it('should render Info with display none when isInfoVisible is false', async() => {
-        const wrapper = createWrapper({ isLoading: false, chatWithUser});
+        const wrapper = createWrapper();
       
         expect(wrapper.find(Info)).toHaveStyleRule('display', 'none');
     })
 
-    it('should render Info with display flex when isInfoVisible is true', () => {
-        const wrapper = createWrapper({ isLoading: false, chatWithUser});
-        wrapper.findByTestid('toggleInfo').simulate('click');
-      
+    it('should render Info with display flex when isInfoVisible is true', async() => {
+        const wrapper = createWrapper();
+        await act(async () => wrapper.findByTestid('toggleInfo').find('button').props().onClick());
+        wrapper.update();
+
         expect(wrapper.find(Info)).toHaveStyleRule('display', 'flex');
     })
 
-    it('should call history push on setChat', () => {
-        const wrapper = createWrapper({ isLoading: false, chatWithUser});
-        wrapper.findByTestid('action').simulate('click');
-
-        expect(pushMock).toHaveBeenCalledWith('/chat');
+    it('should render error', () => {
+        const wrapper = createWrapper('Request not found.');
+      
+        expect(wrapper.find(Error).text()).toBe('Request not found.');
     })
 })
--- src/components/User/tests/unit.test.js
+++ src/components/User/tests/unit.test.js
@@ -1,74 +1,39 @@
 import * as redux from 'react-redux';
 import User from 'components/User/User'
-import LoadingIndicator from 'components/LoadingIndicator/LoadingIndicator'
 import { mount } from 'enzyme';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import { setCurrentChat } from 'app/slices/chatsSlice';
-import { sendRequest } from 'app/slices/requestsSlice';
+import { Error, Info } from 'components/User/UserStyle';
+import 'jest-styled-components';
+import { act } from 'react-dom/test-utils';
+
+jest.mock('components/RequestButtons/RequestButtons', () => () => <div></div>)
 
 describe('User unit tests', () => {
-    let dispatchMock = jest.fn();
-    let history;
-
-    beforeEach(() => {
-        jest.spyOn(redux, 'useDispatch').mockReturnValue(dispatchMock);
-    })
-
-    const createWrapper = (user, state) => {
-        history = createMemoryHistory();
+    const createWrapper = (state) => {
         jest.spyOn(redux, 'useSelector').mockReturnValue(state);
 
         return mount(
-            <Router history={history}>
-                <User page={1} user={{ id: 5, firstName: 'First', lastName: 'Last', profilePicture: 'image.png', ...user}}/>
-            </Router>
+            <User user={{ id: 5, firstName: 'First', lastName: 'Last', profilePicture: 'image.png'}}/>
         )
     }
 
-        
-    it('should call dispatch with setCurrent chat when user has chatWithUser', () => {
-        const chatWithUser = {id: 1, secondUser: { firstName: 'firstname', lastName: 'lastname' }}
-        const wrapper = createWrapper({ chatWithUser, requestState: 'pending' });
-        expect(wrapper.find('button').simulate('click'));
-
-        expect(dispatchMock).toHaveBeenCalledWith(setCurrentChat(chatWithUser));
+    it('should render Info with display none when isInfoVisible is false', async() => {
+        const wrapper = createWrapper();
+      
+        expect(wrapper.find(Info)).toHaveStyleRule('display', 'none');
     })
+
+    it('should render Info with display flex when isInfoVisible is true', async() => {
+        const wrapper = createWrapper();
+        await act(async () => wrapper.findByTestid('toggleInfo').find('button').props().onClick());
+        wrapper.update();
     
-    it('should call dispatch with sendRequest when user does not have chatWithUser ', () => {
-        const wrapper = createWrapper({ chatWithUser: false, requestState: 'send' });
-        expect(wrapper.find('button').simulate('click'));
-
-        expect(dispatchMock).toHaveBeenCalledWith(sendRequest(5));
+        expect(wrapper.find(Info).prop('isInfoVisible')).toBe(true);
+        expect(wrapper.find(Info)).toHaveStyleRule('display', 'flex');
     })
 
-    it('should not call dispatch when requestState is pending', () => {
-        const wrapper = createWrapper({ chatWithUser: false, requestState: 'pending' });
-        expect(wrapper.find('button').simulate('click'));
-
-        expect(dispatchMock).not.toHaveBeenCalled();
-    })
-
-    it('should render Loading when request is loading', () => {
-        const wrapper = createWrapper({ chatWithUser: false }, { isLoading: true });
-        expect(wrapper.find(LoadingIndicator).exists()).toBe(true);
-    })
-
-    it('should render open when user has chatWithUser', () => {
-        const wrapper = createWrapper({ chatWithUser: true });
-        expect(wrapper.find('button').text()).toBe('open');
-    })
-
-    it('should render requestState when user does not have chatWithUser', () => {
-        const wrapper = createWrapper({ chatWithUser: false, requestState: 'pending' });
-        expect(wrapper.find('button').text()).toBe('pending');
-    })
-
-    it('should call history push when setCurrent', () => {
-        const chatWithUser = {id: 1, secondUser: { firstName: 'firstname', lastName: 'lastname' }}
-        const wrapper = createWrapper({ chatWithUser, requestState: 'pending' });
-        expect(wrapper.find('button').simulate('click'));
-
-        expect(history.location.pathname).toBe('/chat');
+    it('should render error', () => {
+        const wrapper = createWrapper('Request not found.');
+      
+        expect(wrapper.find(Error).text()).toBe('Request not found.');
     })
 })
