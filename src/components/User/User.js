20f83a9c4787179dfb08c459df44cea6964d742e
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-25 03:10:45 +0300

    getActionButton added + Container

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,17 +1,17 @@
 import { useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentChat } from 'app/slices/chatsSlice';
-import { sendRequest } from 'app/slices/requestsSlice';
+import { sendRequest, acceptRequest, denyRequest } from 'app/slices/requestsSlice';
 import LoadingIndicator from 'components/LoadingIndicator/LoadingIndicator';
 import { memo } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Container, Image, Info, ButtonsContainer, Button, InfoContainer } from 'components/User/UserStyle';
+import { Container, Image, Info, ButtonsContainer, Button, InfoContainer, UserContainer, Error } from 'components/User/UserStyle';
 import { IMAGE_URL } from 'appConstants';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInfo, faForward } from '@fortawesome/free-solid-svg-icons'
+import { faInfo, faForward, faCheck, faTimes, faShare, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
-const User = memo(({user: { id, profileImage, firstName, lastName, chatWithUser, createdAt, requestState }}) => {
+const User = memo(({user: { id, profileImage, firstName, lastName, chatWithUser, createdAt, requestState, requestId }}) => {
     const history = useHistory();
     const dispatch = useDispatch();
     const request = useSelector(state => state.requests.data[id]);
@@ -23,39 +23,74 @@ const User = memo(({user: { id, profileImage, firstName, lastName, chatWithUser,
         dispatch(setCurrentChat(chatWithUser));
     }
 
-    const manageRequest = () => {
-        if(message != 'pending'){
-            dispatch(sendRequest(id));
-        }
-    }
-
     useEffect(() => {
         if(request && !request.isLoading){
             setMessage(request.state);
         }
     }, [request])
 
+    const accept = () => {
+        dispatch(acceptRequest(requestId));
+    }
+
+    const getActionButton = () => {
+        let action;
+        let icon;
+        switch(message){
+            case 'accept':
+                action = () => dispatch(denyRequest({ id: requestId, requestState: 'accept' }));
+                icon = <FontAwesomeIcon icon={faTimes}/>;
+                break;
+            case 'pending':
+                action = () => dispatch(denyRequest({ id: requestId, requestState: 'pending' }));
+                icon = <FontAwesomeIcon icon={faPaperPlane}/>;
+                break;
+            case 'send':
+                action = () => dispatch(sendRequest(id));
+                icon = <FontAwesomeIcon icon={faShare}/>;
+                break;
+            case 'complete':
+                action = setChat;
+                icon = <FontAwesomeIcon icon={faForward}/>;
+                break;
+        }
+        
+        return(
+            <Button data-testid='action' onClick={action}>
+                {request?.isLoading
+                    ? <LoadingIndicator />
+                    : icon
+                }
+            </Button>
+        )
+    }
+
     return (
         <Container>
-            <Image>
-                <img src={`${IMAGE_URL}/${profileImage}`}/>
-            </Image>
-            <InfoContainer isInfoVisible={isInfoVisible}>
-                <span><b>{`${firstName} ${lastName}`}</b></span>
-                <Info isInfoVisible={isInfoVisible}>
+            <UserContainer>
+                <Image>
+                    <img src={`${IMAGE_URL}/${profileImage}`}/>
+                </Image>
+                <InfoContainer isInfoVisible={isInfoVisible}>
                     <span><b>{`${firstName} ${lastName}`}</b></span>
-                    <span><b>{createdAt}</b></span>
-                </Info>
-            </InfoContainer>
-            <ButtonsContainer>
-                <Button data-testid='toggleInfo' onClick={() => setIsInfoVisible(!isInfoVisible)}><FontAwesomeIcon icon={faInfo}/></Button>
-                <Button data-testid='action' onClick={message == 'open' ? setChat : manageRequest}>
-                    {request?.isLoading
-                        ? <LoadingIndicator />
-                        : message
+                    <Info isInfoVisible={isInfoVisible}>
+                        <span><b>{`${firstName} ${lastName}`}</b></span>
+                        <span><b>{createdAt}</b></span>
+                    </Info>
+                </InfoContainer>
+                <ButtonsContainer>
+                    <Button data-testid='toggleInfo' onClick={() => setIsInfoVisible(!isInfoVisible)}><FontAwesomeIcon icon={faInfo}/></Button>
+                    {message == 'accept' &&
+                        <Button data-testid='accept' onClick={accept}>
+                            {request?.isLoading
+                                ? <LoadingIndicator />
+                                : <FontAwesomeIcon icon={faCheck} />
+                            }
+                        </Button>
                     }
-                </Button>
-            </ButtonsContainer>
+                    {getActionButton()}
+                </ButtonsContainer>
+            </UserContainer>
         </Container>
     )
 })
--- src/components/User/UserStyle.js
+++ src/components/User/UserStyle.js
@@ -7,6 +7,13 @@ export const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+`
+
+export const UserContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
 export const Info = styled.div`
