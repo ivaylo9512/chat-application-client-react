c92e77d09e3968bc1bb8fd0aa6a9f0770a283747
ivaylo <ivayloaleksandrov@yahoo.com>
2021-09-02 07:57:12 +0300

    more saga users tests + fix for coverage output folder change

--- src/app/sagas/tests/users.test.js
+++ src/app/sagas/tests/users.test.js
@@ -3,6 +3,8 @@ import { call, select } from 'redux-saga/effects';
 import { BASE_URL } from 'appConstants';
 import usersReducer, { getUsersData, onUsersComplete, onUsersError } from 'app/slices/usersSlice';
 import { getUsers } from 'app/sagas/users';
+import { onLogout } from 'app/slices/authenticateSlice';
+import { wrapper } from 'app/sagas/index.js';
 
 describe('users saga tests', () => {
     it('should set state on users request with split array on take count in state', () => {
@@ -20,7 +22,6 @@ describe('users saga tests', () => {
                 maxPages: 2,
                 data: [ currentData ],
                 lastData,
-                currentChat: null,
                 currentData,
                 currentPage: 1
             },
@@ -62,9 +63,7 @@ describe('users saga tests', () => {
                 [call(fetch, `${BASE_URL}/api/users/auth/searchForUsers/${query.take * query.pages}/${lastData.firstName} ${lastData.lastName}/${lastData.id}`, {
                     headers:{
                         Authorization: null
-                    }}), 
-                    new Response(JSON.stringify(pageable), { status: 200 })
-                ],
+                }}), new Response(JSON.stringify(pageable), { status: 200 })],
                 [select(getUsersData), state.dataInfo]
             ])
             .put(onUsersComplete(completePayload))
@@ -76,7 +75,6 @@ describe('users saga tests', () => {
                     data: [...state.dataInfo.data, ...completePayload.pageable.data],
                     currentData: completePayload.pageable.data[1],
                     lastData: completePayload.pageable.data[1][2],
-                    currentChat: null
                 },
                 error: null,
                 isLoading: false,
@@ -127,4 +125,74 @@ describe('users saga tests', () => {
             .run()
     })
 
+    it('should set state on users request with initial state', () => {
+        const query = {
+            take: 3,
+            direction: 'ASC',
+            pages: 2,
+            name: ''
+        }
+
+        const pageable = {
+            data: [{ id: 4, firstName: 'd', lastName: 'd' }, { id: 5, firstName: 'e', lastName: 'e' }, { id: 6, firstName: 'f', lastName: 'f' }, 
+                { id: 7, firstName: 'g', lastName: 'g' }, { id: 8, firstName: 'h', lastName: 'h' }, { id: 9, firstName: 'j', lastName: 'j' }],
+            count: 12
+        }
+
+        const completePayload = {
+            pageable: {
+                data: [[ pageable.data[0], pageable.data[1], pageable.data[2] ], 
+                    [ pageable.data[3], pageable.data[4], pageable.data[5] ]],
+                lastUser: pageable.data[5],
+                pages: 4,
+                count: 12
+            },
+            query
+        }
+        return expectSaga(getUsers, { payload: query })
+            .withReducer(usersReducer)
+            .provide([
+                [call(fetch, `${BASE_URL}/api/users/auth/searchForUsers/${query.take * query.pages}`, {
+                    headers:{
+                        Authorization: null
+                }}), new Response(JSON.stringify(pageable), { status: 200 })],
+                [select(getUsersData), { lastData: null }]
+            ])
+            .put(onUsersComplete(completePayload))
+            .hasFinalState({
+                dataInfo: {
+                    pages: 2,
+                    maxPages: 4,
+                    currentPage: 2,
+                    data: completePayload.pageable.data,
+                    currentData: completePayload.pageable.data[1],
+                    lastData: completePayload.pageable.data[1][2],
+                },
+                error: null,
+                isLoading: false,
+                query
+            })
+            .run();
+    })
+
+    it('should call onLogout on users request error with 401', () => {
+        const query = {
+            take: 2,
+            direction: 'ASC',
+            pages: 1
+        }
+
+        return expectSaga(wrapper(getUsers), { payload: query })
+            .withReducer(usersReducer)
+            .provide([
+                [call(fetch, `${BASE_URL}/api/users/auth/searchForUsers/2`, {
+                    headers:{
+                        Authorization: null
+                    }
+                }), new Response('Jwt token has expired.', { status: 401 })],
+                [select(getUsersData), { data: { lastData: null } }]
+            ])
+            .put(onLogout('Session has expired.'))
+            .run()
+    })
 })
